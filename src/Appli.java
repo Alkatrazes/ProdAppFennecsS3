a3893679281d92066c13cbebdf74e19cda2840df
bchanez <bastien.chanez@gmail.com>
2016-11-04 17:15:38 +0100

    Update Appli.java

--- src/Appli.java
+++ src/Appli.java
@@ -1,16 +1,13 @@
 import controller.ControlGroup;
 import model.Jeu;
 
-import java.io.IOException;
-import java.sql.SQLException;
-
 /**
  * Created by bastien on 22/09/16.
  */
 
 public class Appli {
 
-    public static void main(String[] args) throws IOException, SQLException {
+    public static void main(String[] args) {
 
         Jeu jeu = new Jeu();
         ControlGroup controlGroup = new ControlGroup(jeu);
