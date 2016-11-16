16cca209a1b7114b5414bc62095c68d9fc135d51
Bastien Chanez <bastien.chanez@gmail.com>
2016-11-16 23:12:02 +0100

    correction assert

--- test/CarteUnitTest.java
+++ test/CarteUnitTest.java
@@ -10,13 +10,13 @@ public class CarteUnitTest {
     @Test
     public void testPourUneCarteA1Valeur() {
         Carte c = new Carte(1);
-        Assert.assertEquals(1,c.getValeur(),0);
+        Assert.assertEquals(1,c.getValeur());
     }
 
     @Test
     public void testPourUneCarteA2Valeur() {
         Carte c = new Carte(1, 5);
-        Assert.assertEquals(1,c.getValeur(0),0);
-        Assert.assertEquals(5,c.getValeur(1),0);
+        Assert.assertEquals(1,c.getValeur(0));
+        Assert.assertEquals(5,c.getValeur(1));
     }
 }
