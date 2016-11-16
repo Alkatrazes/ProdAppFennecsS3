3e40a3d6e4d01ba978ea10639ac6d11a0ccfc38b
Bastien Chanez <bastien.chanez@gmail.com>
2016-11-16 20:35:47 +0100

    test des cartes à 1 valeur et 2 valeurs

--- src/model/Carte.java
+++ src/model/Carte.java
@@ -4,4 +4,23 @@ package model;
  * Created by bastien on 04/11/16.
  */
 public class Carte {
+    private int valeur [];
+
+
+    public Carte(int valeur) {
+        this.valeur = new int[]{valeur};
+    }
+
+    public Carte(int valeur1, int valeur2) {
+        this.valeur = new int[]{valeur1, valeur2};
+    }
+
+
+    public int getValeur() {
+        return valeur[0];
+    }
+
+    public int getValeur(int indiceValeur) {
+        return valeur[indiceValeur];
+    }
 }
---
+++ test/CarteUnitTest.java
@@ -0,0 +1,22 @@
+import model.Carte;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by bastien on 16/11/16.
+ */
+public class CarteUnitTest {
+
+    @Test
+    public void testPourUneCarteA1Valeur() {
+        Carte c = new Carte(1);
+        Assert.assertEquals(1,c.getValeur(),0);
+    }
+
+    @Test
+    public void testPourUneCarteA2Valeur() {
+        Carte c = new Carte(1, 5);
+        Assert.assertEquals(1,c.getValeur(0),0);
+        Assert.assertEquals(5,c.getValeur(1),0);
+    }
+}
